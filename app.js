af1f87dc9775fda8e883b183b0a976ba967893d0
KVG24 <141866132+KVG24@users.noreply.github.com>
2023-10-25 10:58:39 +0300

    Fix clear button and change UI

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
 const container = document.querySelector('.container');
-const divColor = 'rgb(201, 201, 201'
+const divColor = 'rgb(201, 201, 201)'
+const clearBtn = document.getElementById('clr-btn');
 let paintColor = 'black';
 
-
 function generateGrid(num) {
-    let divWidth = 40 / num + 'rem';
-    let divHeigth = 40 / num + 'rem';
+    let divWidth = 50 / num + 'rem';
+    let divHeigth = 50 / num + 'rem';
     for (i = 0; i < (num * num); i++) {
         let newDiv = document.createElement('div');
         newDiv.className = 'divs';
         newDiv.style.width = divWidth;
         newDiv.style.height = divHeigth;
+        newDiv.setAttribute('draggable', false);
         container.appendChild(newDiv);
-        newDiv.addEventListener('mouseover', color);
-        container.addEventListener('contextmenu', (event) => {event.preventDefault()});
+        newDiv.addEventListener('mouseover', color)
     }
+    container.addEventListener('contextmenu', (event) => {event.preventDefault()});
 }
 
 generateGrid(16);
@@ -31,10 +32,13 @@ function generateNew() {
 }
 
 function clear() {
-    let board = document.querySelector('.container');
-    let cells = board.querySelectorAll('div');
-    cells.forEach((cell) => cell.style.backgroundColor = divColor);
+    let cells = container.querySelectorAll('div');
+    for (const cell of cells) {
+        cell.style.backgroundColor = divColor
+    }
 }
+
+clearBtn.addEventListener('click', clear);
 
 function color() {
     if (paintColor === 'random') {
